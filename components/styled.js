4b595ffaf8c0af02becabb2cd4dbe00ef6a64c5b
artif1c1al <zhuravlenkoartem@gmail.com>
2020-10-21 00:53:30 +0300

    Posts markdown added

---
+++ components/styled.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  max-width: 1000px;
+  margin: 20px auto;
+  background-color: #fafafa;
+  height: 100vh;
+`;
+
+export const PostHeader = styled.h2``;
+
+export const Subtitle = styled.p`
+  color: #777;
+`;
+
+export const Author = styled.p`
+  color: #777;
+`;
+
+export const CreationDate = styled.p`
+  color: #777;
+`;
+
+export const PostImg = styled.img`
+  width: 100px;
+`;
+
+export const Post = styled.div`
+  display: grid;
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,22 @@
+import {
+  Wrapper,
+  PostHeader,
+  Subtitle,
+  Author,
+  CreationDate,
+  PostImg,
+} from "../components/styled";
+
 export default function Index() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Wrapper>
+        <PostHeader>News</PostHeader>
+        <Subtitle>How are you?</Subtitle>
+        <Author>Ivan Ivanov</Author>
+        <CreationDate>Oct 15 </CreationDate>
+        <PostImg src="https://miro.medium.com/max/750/0*pGHUnhhI9fiv3RcS.jpg" />
+      </Wrapper>
+    </div>
+  );
 }
