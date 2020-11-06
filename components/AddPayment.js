425906021980566609836e036c21f94de9d89587
artif1c1al <zhuravlenkoartem@gmail.com>
2020-11-06 12:48:30 +0300

    rerender bugfix

--- components/AddPayment.js
+++ components/AddPayment.js
@@ -14,27 +14,45 @@ import PaypalForm from "./paymentMethods/PaypalForm";
 export default function AddPayment() {
   const { paymentMethod } = useContext(Context);
 
-  const RenderPayment = () => {
-    switch (paymentMethod) {
-      case "Кредитная/дебетовая карта":
-        return <CardForm />;
-      case "PayPal":
-        return <PaypalForm />;
-      default:
-        return (
-          <>
-            <StyledTextboxInfo>
-              Оплата на сайте {paymentMethod}
-            </StyledTextboxInfo>
-          </>
-        );
-    }
-  };
+  // const RenderPayment = (paymentMethod) => {
+  //   // switch (paymentMethod) {
+  //   //   case "Кредитная/дебетовая карта":
+  //   //     return <CardForm />;
+  //   //   case "PayPal":
+  //   //     return <PaypalForm />;
+  //   //   default:
+  //   //     return (
+  //   // <>
+  //   //   <StyledTextboxInfo>
+  //   //     Оплата на сайте {paymentMethod}
+  //   //   </StyledTextboxInfo>
+  //   // </>
+  //   //     );
+  //   // }
+  //   if (paymentMethod === "Кредитная/дебетовая карта") {
+  //     return <CardForm />;
+  //   }
+  //   return <PaypalForm />;
+  //   // if (paymentMethod === "PayPal") {
+  //   // }
+  // };
+
+  const SiteRedirect = () => (
+    <>
+      <StyledTextboxInfo>Оплата на сайте {paymentMethod}</StyledTextboxInfo>
+    </>
+  );
 
   return (
     <StyledAddCard>
       <SelectCard />
-      <RenderPayment />
+      {paymentMethod === "PayPal" ? (
+        <PaypalForm />
+      ) : paymentMethod === "Кредитная/дебетовая карта" ? (
+        <CardForm />
+      ) : (
+        <SiteRedirect />
+      )}
     </StyledAddCard>
   );
 }
--- components/paymentMethods/PaypalForm.js
+++ components/paymentMethods/PaypalForm.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { StyledEmailInput } from "../styled";
+import { Context } from "../../pages/index";
 
 export default function PaypalForm() {
+  const { paypalEmail, setPaypalEmail } = useContext(Context);
+  const onChange = (e) => {
+    setPaypalEmail(e.target.value);
+  };
   return (
     <label htmlFor="">
       Введит ваш PayPal:
-      <StyledEmailInput placeholder="e-mail" />
+      <StyledEmailInput
+        value={paypalEmail}
+        onChange={onChange}
+        placeholder="e-mail"
+      />
     </label>
   );
 }
